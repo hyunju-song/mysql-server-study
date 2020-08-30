1dd6d12a9ed093594564b5dc7c134482e5a491d6
hyunjusong <hun3780@gmail.com>
2020-08-30 14:36:24 +0900

    unexpected token C 에러해결

--- client/app.js
+++ client/app.js
@@ -6,138 +6,142 @@ let postMessage = {};
 let dataIdArr = [];
 let id = 0;
 //지금시간 구하기
-function newTime(){
+function newTime() {
   let date = new Date();
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
   let nowTime = `${hours < 10 ? `0${hours}` : hours}:${
-      minutes < 10 ? `0${minutes}` : minutes}:${
-      seconds < 10 ? `0${seconds}` : seconds}`;
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
   return nowTime;
 }
 //지금의 날짜와 시간 구하기
-function getToday(){
+function getToday() {
   let date = new Date();
-  let nowDate = date.getFullYear()+"-"+(
-      "0"+(date.getMonth()+1)).slice(-2)+"-"+(
-      "0"+date.getDate()).slice(-2) + " " + newTime();
+  let nowDate =
+    date.getFullYear() +
+    "-" +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    "-" +
+    ("0" + date.getDate()).slice(-2) +
+    " " +
+    newTime();
   return nowDate;
 }
 let message = document.querySelector("#message-input");
 let user = document.querySelector("#input-username");
-let room = document.querySelector("#input-room"); 
+let room = document.querySelector("#input-room");
 let submit = document.querySelector("#submit");
 
 const app = {
-  server: 'http://localhost:3000/classes/messages',
-  init : () => {
+  server: "http://localhost:3000/classes/messages",
+  init: () => {
     fetch(app.server)
-    .then((response) => response.json())
-    .then(json => {
-      app.roomnameIsNew(json);
-    })
+      .then((response) => response.json())
+      .then((json) => {
+        app.roomnameIsNew(json);
+      });
   },
-  fetch : ()=>{
+  fetch: () => {
     window
-      .fetch(app.server , {
-        headers : { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-         }
+      .fetch(app.server, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
       })
       .then((response) => {
-        return response.json()})
+        return response.json();
+      })
       .then((data) => {
         //console.log(data)
-        return data.filter((elem)=>{
-        if(elem.roomname === roomNameOption[document
-          .querySelector('select').selectedIndex]){
-          return true;
-        }
-      })})
+        return data.filter((elem) => {
+          if (
+            elem.roomname ===
+            roomNameOption[document.querySelector("select").selectedIndex]
+          ) {
+            return true;
+          }
+        });
+      })
       .then((con) => {
         //console.log(con)
         app.clearMessages();
-        for(let i=0;i<con.length;i++){
+        for (let i = 0; i < con.length; i++) {
           app.renderMessage(con[i]);
         }
       });
   },
-  send : (postMessage) => {
+  send: (postMessage) => {
     window
-      .fetch(app.server,{
-        method: 'POST',
+      .fetch(app.server, {
+        method: "POST",
         body: JSON.stringify(postMessage),
         headers: {
           "Content-Type": "application/json",
-        }
+        },
       })
-      .then(res => res.json())
+      .then((res) => res.json()) //!서버에서 데이터를 보내줄때, json 형태로 res가 오지 않았기 때문에 파싱할게 없어서 에러가 난것. 그래서 포스트
       .then(app.fetch()) //실질적으로 두번 fetch
       .then(app.clearForm());
     //console.log(JSON.stringify(postMessage))
   },
-  clearMessages : function(){
-    document.querySelector('#chats').innerHTML = '';
+  clearMessages: function () {
+    document.querySelector("#chats").innerHTML = "";
   },
-  clearForm : function(){
+  clearForm: function () {
     message.value = "";
     user.value = "";
     room.value = "";
   },
-  renderMessage : function({username, text, roomname, id}){
-    ///</ , />/ 이건 정규표현식의 일종으로 태그로 인식되는 < 나 > 가 입력되는 경우에 
+  renderMessage: function ({ username, text, roomname, id }) {
+    ///</ , />/ 이건 정규표현식의 일종으로 태그로 인식되는 < 나 > 가 입력되는 경우에
     //&lt; 나 &gt;로 변환해준다는 말
     let tmpl = `<div class="dataId" data-id="${id}">
       <p class="username"> username: ${username
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-      }</p>
-      <p> text: ${text
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-      }</p>
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")}</p>
+      <p> text: ${text.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</p>
       <p> date: ${getToday()}</p>
-      <p>roomname: ${roomname
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-      }</p>
+      <p>roomname: ${roomname.replace(/</g, "&lt;").replace(/>/g, "&gt;")}</p>
     </div>`;
 
-    document.querySelector('#chats').innerHTML =
-      tmpl + document.querySelector('#chats').innerHTML;
-    
-    let dataid = document.querySelector('#chats').firstChild.dataset.id;
+    document.querySelector("#chats").innerHTML =
+      tmpl + document.querySelector("#chats").innerHTML;
+
+    let dataid = document.querySelector("#chats").firstChild.dataset.id;
     dataIdArr.push(dataid);
   },
-  addRoomName : function(data){
+  addRoomName: function (data) {
     const optionTmpl = `<select>
       <option class="roomname-option"
       value = ${data}
       type = "button"> ${data} 
       </option>
-      </select>`
-    let select = document.querySelector('select');
+      </select>`;
+    let select = document.querySelector("select");
     select.innerHTML = select.innerHTML + optionTmpl;
     //select.innerHTML = optionTmpl +select.innerHTML
-    select.onchange = function(){
-      app.fetch()
+    select.onchange = function () {
+      app.fetch();
       //app.clearForm()
-    }
+    };
     //callback();
-   },
-   roomnameIsNew : res => {
-     res.forEach((elem) =>{
-       app.renderMessage(elem);
-       if(!roomNameOption.includes(elem.roomname)){
-         roomNameOption.push(elem.roomname);
-         app.addRoomName(elem.roomname , ()=>{app.fetch()});
-       }
-     })
+  },
+  roomnameIsNew: (res) => {
+    res.forEach((elem) => {
+      app.renderMessage(elem);
+      if (!roomNameOption.includes(elem.roomname)) {
+        roomNameOption.push(elem.roomname);
+        app.addRoomName(elem.roomname, () => {
+          app.fetch();
+        });
+      }
+    });
     return;
-   },
-  handleSubmit: e => {
+  },
+  handleSubmit: (e) => {
     e.preventDefault();
     app.clearMessages();
     postMessage.username = user.value;
@@ -148,37 +152,40 @@ const app = {
     //   postMessage.roomname = roomNameOption[document
     //     .querySelector('select').selectedIndex];
     // }
-    if(!room.value){
-      postMessage.roomname = roomNameOption[document.querySelector('select').selectedIndex]
-      room.value = postMessage.roomname //빈 룸네임이 자꾸 추가돼서
+    if (!room.value) {
+      postMessage.roomname =
+        roomNameOption[document.querySelector("select").selectedIndex];
+      room.value = postMessage.roomname; //빈 룸네임이 자꾸 추가돼서
     } else {
       postMessage.roomname = room.value;
     }
     postMessage.date = getToday();
     postMessage.id = id;
-    id ++;
-    console.log(postMessage)
-    if(!roomNameOption.includes(room.value)){
+    id++;
+    console.log(postMessage);
+    if (!roomNameOption.includes(room.value)) {
       roomNameOption.push(room.value);
-      app.addRoomName(room.value 
-        // , 
+      app.addRoomName(
+        room.value
+        // ,
         // ()=>{
         //   app.fetch()
         //   app.clearForm()
         // }
-        );
+      );
     }
-    app.send(postMessage 
+    app.send(
+      postMessage
       // ()=>{
       //   app.fetch()
       //   //.then(app.clearForm());
       //   app.clearForm()
       // } // callback을 못읽어줘서 then으로 처리
     );
-  }
+  },
 };
 
-submit.addEventListener('click', app.handleSubmit);
+submit.addEventListener("click", app.handleSubmit);
 app.init();
 
 // let data = document.querySelector('#chats').firstChild
@@ -188,22 +195,21 @@ app.init();
 //autofetch 부분
 let idHas = false;
 
-function autoFetch(){
+function autoFetch() {
   fetch(app.server)
-  .then(res => res.json())
-  .then(json => {
-    app.fetch();
-    if(idHas){
-      let jsonId = json[0]['id'];
-      let dataLastId = dataIdArr[0];
-      if(jsonId === dataLastId){
-        return;
+    .then((res) => res.json())
+    .then((json) => {
+      app.fetch();
+      if (idHas) {
+        let jsonId = json[0]["id"];
+        let dataLastId = dataIdArr[0];
+        if (jsonId === dataLastId) {
+          return;
+        }
+        setTimeout(autoFetch, 5000);
+        idHas = true;
       }
-      setTimeout(autoFetch, 5000);
-      idHas = true;
-    }
-  })
+    });
 }
 
 autoFetch();
-
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,58 +1,56 @@
 /*eslint-disable*/
-var models = require('../models');
+var models = require("../models");
 //const { messages } = require('../models');
-
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      models.messages.get(function(err, results){
-        if(err){
-          res.send(err)
+      models.messages.get(function (err, results) {
+        if (err) {
+          res.send(err);
         } else {
-        res.json(results)
+          res.json(results);
         }
       });
       //res.status(200).send(message);
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      var params = [req.body.roomname, req.body.text, req.body.username]
-      var users = [req.body.username]
-      models.messages.post(params, users, function(err){
-        if(err){
-          res.send(err)
+      var params = [req.body.roomname, req.body.text, req.body.username];
+      var users = [req.body.username];
+      models.messages.post(params, users, function (err) {
+        if (err) {
+          res.send(err);
           //여기에 send(404)를 써주니까 head에러가 발생했다. 그 이유는 send 하고 난 이후에 아래에서 다시 status설정해주는 코드가 나온느데 send 이후로는 헤더 작성이 불가능하다.
           //=> 결국 센드 진행함 하지만, 헤더 설정을 해주고 나서, send를 해줘야 한다. 그리고 if구문으로 헤더설정했으면, 밑에 else로 빼줘야한다. 지금은 헤더가 두번설정되고 있다
-         } else {
-          res.sendStatus(201)
-         }
-      })
+        } else {
+          //res.sendStatus(201)
+          res.send(201).json({ id: 0 }); // 보내주는 데이터를 json 처리를 해주는 것이 핵심. 보통은 id 값을 기준으로 불러와서 해주는데 이것이 정답은 아니다.
+        }
+      });
       //res.status(201).send('success');
-    } // a function which handles posting a message to the database
+    }, // a function which handles posting a message to the database
   },
 
   users: {
     get: function (req, res) {
-      models.users.get(function(err,results){
-        if(err){
+      models.users.get(function (err, results) {
+        if (err) {
           res.send(err);
         }
-        res.json(results)
+        res.json(results);
       });
       //res.status(200).send(user);
     }, // a function which handles a get request for all users
     post: function (req, res) {
       var params = [req.body.username];
-      models.users.post(params, function(err){
-        if(err){
-          res.send(err)
+      models.users.post(params, function (err) {
+        if (err) {
+          res.send(err);
         } else {
-        res.sendStatus(201)
+          res.sendStatus(201);
         }
       });
       //res.status(201).send('success');
-    } // a function which handles posting a user to the database
-  }
+    }, // a function which handles posting a user to the database
+  },
 };
-
-
